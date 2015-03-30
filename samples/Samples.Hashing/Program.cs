7f34d449a9ea0a490bd66713db11a1b0f358f4c1
Alistair Singh <alistair.singh7@gmail.com>
2015-03-30 21:38:34 +0200

    added try catch

--- samples/Samples.Hashing/Program.cs
+++ samples/Samples.Hashing/Program.cs
@@ -29,28 +29,36 @@ namespace Sample.Hashing
 
       using (Benchmark.Start("*"))
       {
-        List<IStatement> ast;
-        using (Benchmark.Start("parse"))
-          ast = TSql.ParseFile("Test.sql").ToList();
-
-
-        using (HashAlgorithm hash = MD5.Create())
-        using (var stream = new MemoryStream())
-        using (var writer = new StreamWriter(stream))
+        try
         {
-          using (Benchmark.Start("format"))
+          List<IStatement> ast;
+          using (Benchmark.Start("parse"))
+            ast = TSql.ParseFile("Test.sql").ToList();
+
+
+          using (HashAlgorithm hash = MD5.Create())
+          using (var stream = new MemoryStream())
+          using (var writer = new StreamWriter(stream))
           {
-            ast.Write(writer);
-            writer.Flush();
+            using (Benchmark.Start("format"))
+            {
+              ast.Write(writer);
+              writer.Flush();
+            }
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            byte[] hashBytes;
+            using (Benchmark.Start("hash"))
+              hashBytes = hash.ComputeHash(stream);
+
+            Console.WriteLine("Hash is '{0}'", Format(hashBytes));
           }
-
-          stream.Seek(0, SeekOrigin.Begin);
-
-          byte[] hashBytes;
-          using (Benchmark.Start("hash"))
-            hashBytes = hash.ComputeHash(stream);
-
-          Console.WriteLine("Hash is '{0}'", Format(hashBytes));
+        }
+        catch (Exception e)
+        {
+          using (ConsoleUtil.Color(ConsoleColor.Red))
+            Console.WriteLine(e.Message);
         }
       }
       Console.WriteLine("Press any key to quit...");
