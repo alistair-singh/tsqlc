8bc708be295d85a16c997a3f6981408bf2d000fe
Alistair Singh <alistair.singh7@gmail.com>
2015-03-30 21:51:02 +0200

    sample projects restructure

---
+++ samples/Sample.CodeAnalysis/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tsqlc.Util;
+
+namespace Sample.CodeAnalysis
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Benchmark.Begin += (name, id) =>
+      {
+        using (ConsoleUtil.Color(fg: ConsoleColor.Green))
+          Console.WriteLine("{0}:{1}->{2}", id, name, DateTime.Now);
+      };
+
+      Benchmark.End += (name, id, elapse) =>
+      {
+        using (ConsoleUtil.Color(fg: ConsoleColor.Green))
+          Console.WriteLine("{0}:{1}->{2}ms, {3}", id, name, elapse, DateTime.Now);
+      };
+
+      using(Benchmark.Start("*"))
+      {
+        try
+        {
+          //TODO: Implement
+        }
+        catch(Exception e)
+        {
+          using (ConsoleUtil.Color(ConsoleColor.Red))
+            Console.WriteLine(e.Message);
+        }
+      }
+      Console.WriteLine();
+      Console.ReadKey(true);
+    }
+  }
+}
